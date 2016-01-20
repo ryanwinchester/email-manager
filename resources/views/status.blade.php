537f8ba5d32e8f01e801c48301cf9873c7da68b3
ryanwinchester <fungku@gmail.com>
2016-01-20 13:57:24 -0800

    Add confirm buttons and loading animations

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+@endsection
+
 @section('content')
 <div class="container">
 
@@ -15,7 +19,7 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-sm-8">
-                            <form action="{{ route('email.change', [$email]) }}" method="POST">
+                            <form id="change-email-form" action="{{ route('email.change', [$email]) }}" method="POST">
                                 {{ csrf_field() }}
                                 {{ method_field('PATCH') }}
                                 <div class="form-group">
@@ -23,19 +27,19 @@
                                     <input type="email" name="new_email" id="new_email" placeholder="New email" class="form-control" required>
                                 </div>
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-warning">Change</button>
+                                    <button type="submit" class="btn btn-warning" data-loading-text="Loading..." autocomplete="off">Change</button>
                                 </div>
                             </form>
                         </div>
                         <div class="col-sm-4" style="text-align:center;">
-                            <form action="{{ route('email.unsubscribe', [$email]) }}" method="POST">
+                            <form id="unsubscribe-form" action="{{ route('email.unsubscribe', [$email]) }}" method="POST">
                                 {{ csrf_field() }}
                                 {{ method_field('PATCH') }}
                                 <div class="form-group">
                                     <label>Unsubscribe this user from all lists</label>
                                 </div>
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-danger btn-lg">UNSUBSCRIBE ALL</button>
+                                    <button type="submit" class="btn btn-danger btn-lg" data-loading-text="Loading..." autocomplete="off">UNSUBSCRIBE ALL</button>
                                 </div>
                             </form>
                         </div>
@@ -86,4 +90,77 @@
     </div>
 
 </div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+    <script>
+        $(document).ready(function () {
+
+            $('#change-email-form').submit(function (e) {
+                e.preventDefault();
+                var $this = $(this);
+                var $button = $this.find('button[type=submit]');
+                disableButton($button);
+                swal({
+                    title: "Ground Control to Major Tom!",
+                    text: "Do you really want to change <b>{{ $email }}</b> to <b>" + $this.find('#new_email').val() + "</b>?",
+                    type: "warning",
+                    html: true,
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, change it!",
+                    cancelButtonText: "Nevermind.",
+                    closeOnConfirm: false,
+                    showLoaderOnConfirm: true
+                }, function(confirmed) {
+                    if (confirmed) {
+                        e.target.submit();
+                        setTimeout(function(){
+                            // wait 20 seconds
+                        }, 20000);
+                    } else {
+                        enableButton($button);
+                    }
+                });
+            });
+
+            $('#unsubscribe-form').submit(function (e) {
+                e.preventDefault();
+                var $this = $(this);
+                var $button = $this.find('button[type=submit]');
+                disableButton($button);
+                swal({
+                    title: "Are you sure?",
+                    text: "Do you really want to permanently <em>unsubscribe</em> <b>{{ $email }}</b>? This can't be undone.",
+                    type: "warning",
+                    html: true,
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Just do it!",
+                    cancelButtonText: "Forget it...",
+                    closeOnConfirm: false,
+                    showLoaderOnConfirm: true
+                }, function(confirmed) {
+                    if (confirmed) {
+                        e.target.submit();
+                        setTimeout(function(){
+                            // wait 20 seconds
+                        }, 20000);
+                    } else {
+                        enableButton($button);
+                    }
+                });
+            });
+
+            function disableButton(btn) {
+                btn.button('loading');
+            }
+
+            function enableButton(btn) {
+                btn.button('reset');
+            }
+
+        });
+    </script>
 @endsection
